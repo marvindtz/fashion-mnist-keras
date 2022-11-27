3822f2589197d9bb22960db4baceebc323d6074a
marvindtz <marvin@dietzm.de>
2022-11-27 12:36:52 +0000

    finished model training

--- main.py
+++ main.py
@@ -16,12 +16,26 @@ fashion_mnist = tf.keras.datasets.fashion_mnist
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-#train_images = train_images.reshape(60000, 784)
+train_images = train_images.reshape(60000, 784)
+test_images = test_images.reshape(test_images.shape[0], 784)
 print(train_images.shape)
 
-plt.figure()
-plt.imshow(train_images[0])
-plt.colorbar()
-plt.grid(False)
-plt.show()
+train_images = train_images / 255.0
+test_images = test_images / 255.0
 
+model = tf.keras.Sequential([
+    tf.keras.layers.Input(shape=(784)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10)
+])
+
+model.summary()
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+print('\nTest accuracy:', test_acc)
